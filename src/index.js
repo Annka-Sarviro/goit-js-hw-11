418c3195af6f20f125debea3e1cedf0291fdbc49
Ann Sarviro <Annka_nuha@i.ua>
2022-05-28 02:33:08 +0300

    common task

---
+++ src/index.js
@@ -0,0 +1,85 @@
+import {fetchApi, PER_PAGES} from './js/api.js';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const refs = {
+    formEl: document.querySelector('.search-form'),
+    boxEl: document.querySelector('.gallery'),
+    buttonEl: document.querySelector('.load-more'),
+}
+let query = '';
+let page = 1;
+let totalPages = 1;
+
+refs.formEl.addEventListener('submit', onSearchClick);
+refs.buttonEl.addEventListener('click', onLoadMoreRender)
+
+
+function onSearchClick (event) {
+    event.preventDefault(); 
+    clearImgBox();  
+    query = event.currentTarget.elements.searchQuery.value;
+
+    if (!query) return;
+
+    currentPage = 1;
+    fetchAndRender(query);
+    refs.formEl.reset();
+}
+
+
+function onLoadMoreRender () {
+    page+=1;
+    
+    if (page * PER_PAGES > totalPages) return Notify.failure("We're sorry, but you've reached the end of search results.");
+
+    fetchAndRender(query);
+}
+
+
+async function fetchAndRender (query) {
+    try {const fetchData = await fetchApi(query, page);
+        if (fetchData.total === 0) {return Notify.failure("Sorry, there are no images matching your search query. Please try again.")}
+        renderList(fetchData);
+        refs.buttonEl.hidden = false;}
+    catch (error) {
+        console.log(error.message);
+    }
+        
+  
+}
+
+
+function renderList ({hits, totalHits }) {
+    totalPages = totalHits;
+        const list = hits.map(
+        ({webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
+        {
+            return `<div class="photo-card">
+            <img src="${webformatURL}" alt="${query}" loading="lazy" width='120' />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes ${likes}</b>
+              </p>
+              <p class="info-item">
+                <b>Views ${views}</b>
+              </p>
+              <p class="info-item">
+                <b>Comments ${comments}</b>
+              </p>
+              <p class="info-item">
+                <b>Downloads ${downloads}</b>
+              </p>
+            </div>
+          </div>`
+        }
+      );
+    
+      refs.boxEl.insertAdjacentHTML('beforeend', list.join(' '));
+    return list;
+}
+
+function clearImgBox() {
+    refs.boxEl.innerHTML = '';
+}
---
+++ src/js/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const URL = 'https://pixabay.com/api/';
+const KEY = '27678833-7153b16b322f2af48ae42bf6d';
+export const PER_PAGES = 40;
+
+
+
+export function fetchApi (query, page) {
+    return axios.get(`${URL}?key=${KEY}&q=${query}&per_page=${PER_PAGES}&page=${page}&image_type=photo&orientation=horizontal&safesearch=true`)
+    .then(({data}) => data)
+}
